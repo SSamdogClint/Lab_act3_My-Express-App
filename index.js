8bde0e718f75d33414390638c82218c8d7f07510
Clint Eroll A. Capondag <capondag.clint117@gmail.com>
2026-02-27 22:30:33 +0800

    I add handling methods that can accept GET and POST request

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
+app.use(express.json()); // Middleware to parse JSON bodies
+// Serve static files from the 'public' folder
+app.use(express.static('public')); 
+
 //Define a route for the home page
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 });
 
 //Start the server
